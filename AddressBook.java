e5655f8901e6c5888596c54bd19a91717a8f16bb
Ujjwal Aakash <ujjwalaakash46ua@gmail.com>
2021-08-22 17:41:11 +0530

    Add only for new contact

--- AddressBook.java
+++ AddressBook.java
@@ -4,53 +4,62 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBook {
-	
+
 	static ArrayList<Contact> addressBook = new ArrayList<>();
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book System!");
 		addContact();
 		editContact();
 		deleteContact();
 	}
+
 	static public void addContact() {
-		int flag=0;
-		while(flag ==0) {
+		int flag = 0;
+		while (flag == 0) {
 			Contact c = new Contact();
-			Scanner sc = new Scanner(System.in); 
+			Scanner sc = new Scanner(System.in);
 			System.out.println("---New Contact---");
 			System.out.println("Enter First Name: ");
 			String firstName = sc.next();
+
+			if (!addressBook.isEmpty()) {
+				for (int i = 0; i < addressBook.size(); i++) {
+					if (addressBook.get(i).equals(firstName)) {
+						System.out.println("Contact Exists!");
+						return;
+					}
+				}
+			}
 			c.setFirstName(firstName);
 			System.out.println("Enter Last Name: ");
 			c.setLastName(sc.next());
 			System.out.println("Enter Address: ");
 			c.setAddress(sc.next());
 			System.out.println("Enter City: ");
-			String cityName=sc.next();
-			c.setCity(cityName);
+			c.setCity(sc.next());
 			System.out.println("Enter State: ");
-			String stateName=sc.next();
-			c.setState(stateName);
+			c.setState(sc.next());
 			System.out.println("Enter Zip code: ");
 			c.setZip(sc.next());
 			System.out.println("Enter Phone: ");
 			c.setPhone(sc.next());
 			addressBook.add(c);
+
 			System.out.println("Want to Add more? Enter 1 for Yes or 2 for No :");
-			// if flag is 0 then only loop will run again.
 			flag = sc.nextInt();
-			
+
 		}
 	}
-	
+
 	static public void editContact() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter First Name to Edit : ");
 		String name = sc.next();
-		int flag2 =0;
-		for(int i =0; i<addressBook.size(); i++) {
-			if( addressBook.get(i).getFirstName() == name ) {
-				flag2=1;
+		int flag2 = 0;
+		for (int i = 0; i < addressBook.size(); i++) {
+			if (addressBook.get(i).getFirstName() == name) {
+				flag2 = 1;
 				System.out.println("---Edit Contact---");
 				System.out.println("Enter First Name: ");
 				addressBook.get(i).setFirstName(sc.next());
@@ -68,24 +77,24 @@ public class AddressBook {
 				addressBook.get(i).setPhone(sc.next());
 			}
 		}
-		if(flag2==0) {
+		if (flag2 == 0) {
 			System.out.println("No Such Contact Found!");
 		}
 	}
-	
+
 	static public void deleteContact() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter First Name to delete : ");
 		String name = sc.next();
-		int flag2 =0;
-		for(int i =0; i<addressBook.size(); i++) {
-			if( addressBook.get(i).getFirstName() == name ) {
-				flag2=1;
+		int flag2 = 0;
+		for (int i = 0; i < addressBook.size(); i++) {
+			if (addressBook.get(i).getFirstName() == name) {
+				flag2 = 1;
 				addressBook.remove(i);
 				System.out.println("Deleted!");
 			}
 		}
-		if(flag2==0) {
+		if (flag2 == 0) {
 			System.out.println("No Such Contact Found!");
 		}
 	}
